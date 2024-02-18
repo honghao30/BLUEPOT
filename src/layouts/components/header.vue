9cbb11fa5fce1e2e43ecbac8313f3bba0f414f78
honghao30 <sunghoo78@gmail.com>
2024-02-18 13:58:31 +0900

    레이아웃 헤더 영역

--- src/layouts/components/header.vue
+++ src/layouts/components/header.vue
@@ -1,5 +1,69 @@
 <template>
-  <div>헤더</div>
+  <header>
+    <div class="header-top__ci">
+      <router-link to="#"
+        class="icon-mypage">
+        <span>마이페이지</span>
+      </router-link>
+      <h1>
+        <router-link to="/">
+          <img src="../../assets/images/common/ci.svg" alt="블루팟" />
+        </router-link>
+      </h1>
+    </div>
+    <div class="navigation__container">
+      <div class="navigation__container--top">
+        <ul class="gnb-list">
+          <li class="store">
+            <router-link to="">
+               <span class="menu-name">매장관리</span>
+               <span class="menu-eng">Store management</span>
+            </router-link>
+          </li>
+          <li class="calculate">
+            <router-link to="">
+               <span class="menu-name">정산관리</span>
+               <span class="menu-eng">Settlement management</span>
+            </router-link>
+            <ul class="sub-list">
+                <li>
+                  <router-link to="">
+                    연결관리
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="">
+                    정산자동화
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="">
+                    정산서작성
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="">
+                    정산서조회
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="">
+                    입금/세금계산서/총괄표
+                  </router-link>
+                </li>                                                                
+            </ul>
+          </li>   
+          <li class="member">
+            <router-link to="">
+               <span class="menu-name">회원관리</span>
+               <span class="menu-eng">Member management</span>
+            </router-link>
+          </li>                  
+        </ul>
+      </div>
+      <div class="sub-top__image-area"></div>
+    </div>
+  </header>
 </template>
 
 <script>  
